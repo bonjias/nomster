b7795e42cc0c434c315611f4bb2a2d8536849fa7
vagrant <bonjias@gmail.com>
2015-08-12 20:08:33 +0000

    Lesson 4

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Nomster::Application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-
+ root 'places#index'
   # Example resource route with options:
   #   resources :products do
   #     member do
