2fe3869806835e7aff2ab183a50a17a2a5707ae0
vagrant <bonjias@gmail.com>
2015-08-19 16:33:55 +0000

    Lesson 25

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,9 +1,9 @@
 class PlacesController < ApplicationController
-	before_action :authenticate_user!, :only => [:new, :create]
+	before_action :authenticate_user!, :only => [:new, :create, :edit, :update]
 
 
 	def index
-		@places = Place.all.paginate(:page => params[:page], :per_page => 2).order('created_at ASC')
+		@places = Place.all.paginate(:page => params[:page], :per_page => 5).order('created_at ASC')
 		end
 
 
@@ -22,12 +22,21 @@ class PlacesController < ApplicationController
 		end
 
 		def edit
-			@place = Place.find(params[:id])
+				@place = Place.find(params[:id])
 
-		end 
+				if @place.user != current_user
+   			 	return render :text => 'Not Allowed', :status => :forbidden
+  				end
+		end
+
 
 		def update
 		@place = Place.find(params[:id])
+
+		if @place.user != current_user
+    	return render :text => 'Not Allowed', :status => :forbidden
+  		end
+  		
   		@place.update_attributes(place_params)
   		redirect_to root_path
 		end
