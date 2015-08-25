b7dd76587f6d7be168a6f8f0ac85ec98ce95a6cf
vagrant <bonjias@gmail.com>
2015-08-25 20:45:32 +0000

    got form on page but have routing error

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Nomster::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
   resources :places do
     resources :comments, :only => :create
+    resources :photos, :only => :create 
   end 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
